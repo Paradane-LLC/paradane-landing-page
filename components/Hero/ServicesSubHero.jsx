8cb22a589d77dfb15c442ef1b6a59885a41637e1
Paradane <github@paradane.com>
2025-08-28 00:05:23 +0500

    Update ServicesSubHero.jsx

--- components/Hero/ServicesSubHero.jsx
+++ components/Hero/ServicesSubHero.jsx
@@ -13,7 +13,7 @@ const ServiceItem = ({label, description, href, icon: Icon}) => {
                 <Icon className="text-3xl text-[#00489c]/80 group-hover:text-[#00489c]" size={24} />
             </div>
             <span className="text-xl font-semibold text-zinc-700 group-hover:text-black flex-shrink-0">{label}</span>
-            <span className="text-base text-zinc-700 group-hover:text-black block mt-2">
+            <span className="text-base text-zinc-700 group-hover:text-black block mt-2 tracking-wider">
                 {description}
             </span>
             <span className="text-zinc-700 group-hover:text-black block mt-2 text-lg">
@@ -28,10 +28,10 @@ const ServicesSubHero = () => {
         <div className="absolute -top-22 right-0 left-0 flex flex-col gap-5 z-10">
             <div className="max-w-7xl mx-auto w-full">
                 <div className="grid grid-cols-4 gap-5" style={{ alignItems: 'start' }}>
-                    <ServiceItem href={"#"} label={"Website Development"} description={"We build fast, secure, and scalable websites tailored to your business needs. From simple landing pages to complex platforms, we deliver seamless digital experiences."} icon={Monitor} />
-                    <ServiceItem href={"#"} label={"Social Media Marketing"} description={"Boost your brand presence with targeted campaigns that drive engagement and growth. We create strategies that connect you with the right audience."} icon={Share2} />
+                    <ServiceItem href={"#"} label={"Website Development"} description={"We build fast, secure, and scalable websites tailored to your business needs. From simple landing pages to complex platforms, we deliver seamless experiences."} icon={Monitor} />
+                    <ServiceItem href={"#"} label={"Social Media Marketing"} description={"Boost your brand presence with our targeted campaigns that drive engagement, sales and growth. We create strategies that connect you with the right audience."} icon={Share2} />
                     <ServiceItem href={"#"} label={"User Interface Design"} description={"Designs that always put users first—intuitive, attractive, and conversion-focused. We craft digital experiences that keep people engaged and coming back."} icon={Palette} />
-                    <ServiceItem href={"#"} label={"Search Engine Optimization"} description={"Rank higher, get noticed, and grow organically with smart SEO strategies. Our approach ensures long-term visibility and measurable business results."} icon={Search} />
+                    <ServiceItem href={"#"} label={"Search Engine Optimization"} description={"Rank higher on Search Engines, get noticed, and grow organically with smart SEO strategies. Our approach ensures long-term visibility and measurable business results."} icon={Search} />
                 </div>
             </div>
         </div>
