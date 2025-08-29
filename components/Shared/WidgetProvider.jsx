e2f6b3179880fe1b57a21db8848d3adb7fb84ba8
Paradane <github@paradane.com>
2025-08-30 01:24:24 +0500

    Update WidgetProvider.jsx

--- components/Shared/WidgetProvider.jsx
+++ components/Shared/WidgetProvider.jsx
@@ -9,19 +9,19 @@ function WidgetProvider() {
       const script = document.createElement('script');
       script.type = 'module';
       script.innerHTML = `
-        window.chatwootSettings = {"position":"right","type":"standard","launcherTitle":"Chat with us"};
-            (function(d,t) {
-                var BASE_URL="https://support.paradane.com";
-                var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-                g.src=BASE_URL+"/packs/js/sdk.js";
-                g.async = true;
-                s.parentNode.insertBefore(g,s);
-                g.onload=function(){
-                window.chatwootSDK.run({
-                    websiteToken: 'piUDZg9xrTfxjPsqQ9bpmLCP',
-                    baseUrl: BASE_URL
-                })
-                }
+        window.chatwootSettings = {"position":"right","type":"expanded_bubble","launcherTitle":"Chat with us"};
+        (function(d,t) {
+            var BASE_URL="https://support.paradane.com";
+            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=BASE_URL+"/packs/js/sdk.js";
+            g.async = true;
+            s.parentNode.insertBefore(g,s);
+            g.onload=function(){
+            window.chatwootSDK.run({
+                websiteToken: 'piUDZg9xrTfxjPsqQ9bpmLCP',
+                baseUrl: BASE_URL
+            })
+            }
         })(document,"script");
       `;
       
