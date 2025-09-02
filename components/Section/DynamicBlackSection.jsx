032f28f91446a6a6a2cc4e2cfda0945cfaabcba7
Paradane <github@paradane.com>
2025-09-03 04:36:01 +0500

    Update DynamicBlackSection.jsx

--- components/Section/DynamicBlackSection.jsx
+++ components/Section/DynamicBlackSection.jsx
@@ -1,7 +1,8 @@
 const DynamicBlackSection = () => {
     return (
-        <div className="min-h-screen header-black-dynamic">
-            <div className="relative inset-0 bg-zinc-900 text-white min-h-screen z-10">
+        <div className="relative min-h-screen header-black-dynamic">
+            <div className="absolute inset-0 -z-10 bg-gradient-to-b from-[#001d3f] to-zinc-900" />
+            <div className="relative inset-0 bg-zinc-900/90 text-white min-h-screen z-10">
                 <div className="flex items-center justify-center py-10">
                     <span className="text-center text-6xl max-w-5xl font-semibold leading-20 tracking-wider">Revolutionize Your Customer Interaction With Dynamic Designs</span>
                 </div>
