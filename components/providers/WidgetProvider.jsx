9eae88a7c72577b3be393bfc039262dfe678fa15
Paradane <github@paradane.com>
2025-09-12 16:06:20 +0500

    Update WidgetProvider.jsx

--- components/providers/WidgetProvider.jsx
+++ components/providers/WidgetProvider.jsx
@@ -1,18 +1,38 @@
-"use client";
+"use client"
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { getCalApi } from "@calcom/embed-react";
 
 function WidgetProvider() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.defer = true;
-    script.src =
-      "https://gc.zohopublic.com/org/899434323/flows/37206000000002163/embed/script";
-    document.body.appendChild(script);
+    const loadAndInitZohoDesk = () => {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.id = "zohodeskasap";
+      script.defer = true;
+      script.innerHTML = `
+        var d=document;
+        s=d.createElement("script"),
+        s.type="text/javascript",
+        s.id="zohodeskasapscript",
+        s.defer=!0,
+        s.src="https://desk.zoho.com/portal/api/web/asapApp/1177430000000401059?orgId=899389701",
+        t=d.getElementsByTagName("script")[0],
+        t.parentNode.insertBefore(s,t),
+        window.ZohoDeskAsapReady=function(s){
+          var e=window.ZohoDeskAsap__asyncalls=window.ZohoDeskAsap__asyncalls||[];
+          window.ZohoDeskAsapReadyStatus
+            ? (s&&e.push(s),e.forEach(s=>s&&s()),window.ZohoDeskAsap__asyncalls=null)
+            : s&&e.push(s)
+        };
+      `;
+      document.body.appendChild(script);
+    };
 
-    // preload Cal
+    // Add a small delay to ensure DOM is ready
+    const timer = setTimeout(loadAndInitZohoDesk, 1000);
+
+    // Preload Cal to improve BookerModal performance
     (async function () {
       const cal = await getCalApi({
         namespace: "schedule-a-meeting",
@@ -20,6 +40,10 @@ function WidgetProvider() {
       });
       cal("preload", { calLink: "team/sales/schedule-a-meeting" });
     })();
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, []);
 
   return null;
