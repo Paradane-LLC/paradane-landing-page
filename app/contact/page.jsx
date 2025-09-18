5db64f4f64cbb11d91eb768960c34de54763db91
Michael Fleck <30557441+Michaelfleck@users.noreply.github.com>
2025-09-18 12:00:29 -0400

    Create page.jsx

---
+++ app/contact/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="min-h-screen bg-zinc-200/40">
+      <h1 className="text-5xl font-bold">Contact Us</h1>
+      <p className="mt-4">We would love to hear from you!</p>
+    </div>
+  )
+}
+
+export default page
